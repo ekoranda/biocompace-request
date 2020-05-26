7f5b2d7e4e2f09aa3c0b0c327a5721e582642dc3
ekoranda1 <ekoranda1@gmail.com>
2020-05-26 15:16:39 -0500

    Updated the model to work with new group names from role mapping

--- src/main/java/us/researchdata/biocompace/request/ui/PopulateGroupMembers.java
+++ src/main/java/us/researchdata/biocompace/request/ui/PopulateGroupMembers.java
@@ -1,41 +1,75 @@
 package us.researchdata.biocompace.request.ui;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
+import java.util.Scanner;
 
 import javax.annotation.Resource;
 import javax.annotation.security.DeclareRoles;
-import javax.annotation.security.PermitAll;
-import javax.annotation.security.RolesAllowed;
-import javax.annotation.security.RunAs;
-import javax.ejb.EJB;
 import javax.ejb.SessionContext;
 import javax.ejb.Stateless;
 import javax.enterprise.event.Observes;
 
 import org.imixs.workflow.engine.UserGroupEvent;
-import org.imixs.workflow.exceptions.AccessDeniedException;
-import org.imixs.workflow.faces.data.WorkflowEvent;
 
 @Stateless()
-@DeclareRoles({ "test"})
+@DeclareRoles( "test")
 public class PopulateGroupMembers {
 	@Resource SessionContext ctx;
     public List<String> onUserGroupEvent(@Observes UserGroupEvent userGroupEvent) {
-	    List<String> customGroups = new ArrayList<String>();
+    	
+    	// create buffer for csvFile
+    	String csvFile = "users.txt";
+        String line = "";
+        String cvsSplitBy = ",";
+
+        // list that contains new groups for the current user
+        List<String> customGroups = new ArrayList<String>();
+        
+        // checks if current user is in role
+        // this doesn't work
 	    if (ctx.isCallerInRole("test")) {
 	        customGroups.add("test");
 	    }
+	    
+	    // get the name of the current user
 	    Principal principal = ctx.getCallerPrincipal();
 	    String name = principal.getName();
-	    if(name.equals("proposer1")) {
-	    	customGroups.add("test");
-	    }
-	    customGroups.add("addedGroup");
+	   
+	    // buffer through the csv file line by line to iterate over each user
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+
+            while ((line = br.readLine()) != null) {
+
+                // use comma as separator
+                String[] users = line.split(cvsSplitBy);
+                String roleName;
+                
+                // find the role of the current user and add it to their groups list
+                if(users[0].equals(name)){
+                	roleName = "group.";
+                	roleName = roleName.concat(users[1]);
+                	roleName = roleName.replace(" ", "");
+                	customGroups.add(roleName);
+                }
+                
+                
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        
+        // add the customGroup to the user's group event
 	    userGroupEvent.setGroups(customGroups);
-	    System.out.println(ctx.isCallerInRole("test"));
+
 	    return customGroups;
 	}
 }
